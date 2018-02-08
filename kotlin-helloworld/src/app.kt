c2d3404f1a0a7f47bd10531a40e7a3d4c81f45c5
r.palvair <r.palvair@gmail.com>
2018-02-08 17:40:08 +0000

    feat : Utiliser une chaine sur plusieurs lignes

--- kotlin-helloworld/src/app.kt
+++ kotlin-helloworld/src/app.kt
@@ -6,10 +6,18 @@ fun main(arg: Array<String>) {
     println("Null uppercase [$nullUpperCase]")
     val lenghtMessage = getLengthMessage(applicationName)
     println("message : $lenghtMessage")
+    println("chaine sur plusieurs lignes " + getMultiLinesString())
 }
 
 fun getApplicationName(): String = "kotlin-helloworld"
 
 fun toUpperCaseSafety(input: String?): String? = input.orEmpty().toUpperCase()
 
-fun getLengthMessage(input:String):String =  "$input.length  is ${input.length}"
+fun getLengthMessage(input: String): String = "$input.length  is ${input.length}"
+
+fun getMultiLinesString(): String {
+    return """
+        Hello
+        World
+        """
+}

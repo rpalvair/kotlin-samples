3a224b66675b2dfc2512ae7deaff76972beae8a4
ruddy palvair <r.palvair@gmail.com>
2018-02-20 19:32:08 +0100

    feat : Manipuler les boucles for feat : Manipuler les list et array

--- kotlin-helloworld/src/controlFlow.kt
+++ kotlin-helloworld/src/controlFlow.kt
@@ -3,6 +3,8 @@ fun main(args: Array<String>) {
     println("maximum is $maximum")
     println("isNumber = " + isNumber(1))
     println("isNumber = " + isNumber("1"))
+    displayListContent(listOf(1, 2, 3))
+    displayArrayContent(Array(5, { i -> (i * i) }))
 }
 
 
@@ -12,4 +14,16 @@ fun max(a: Int, b: Int) = if (a > b) a else b
 fun isNumber(a: Any): Boolean = when (a) {
     is Int -> true
     else -> false
+}
+
+fun displayListContent(collection: Collection<Int>) {
+    for (item in collection) {
+        println("item $item")
+    }
+}
+
+fun displayArrayContent(array: Array<Int>) {
+    for (item in array) {
+        println("item $item")
+    }
 }

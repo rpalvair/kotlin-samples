eeba9e22fc483703a87f447105641e4d7a7241de
ruddy palvair <r.palvair@gmail.com>
2018-02-20 22:43:08 +0100

    feat : Manipuler les constructeurs primaires et secondaires

---
+++ kotlin-helloworld/src/User.kt
@@ -0,0 +1,18 @@
+class User(nom: String, val prenom: String?) {
+    val nom = nom.toUpperCase()
+
+    init {
+        val prenomToDisplay = this.prenom ?: ""
+        println("Utilisateur $prenomToDisplay $nom créé")
+    }
+
+    constructor(nom: String) : this(nom,null)
+}
+
+
+fun main(arg: Array<String>) {
+    val user = User("Doe")
+    println("nom = ${user.nom}")
+    println("nom = ${user.prenom}")
+    User("Doe","John")
+}

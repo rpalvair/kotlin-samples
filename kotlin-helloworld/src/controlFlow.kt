f4a60198eadd225136380d3d83c98ae624b06424
ruddy palvair <r.palvair@gmail.com>
2018-02-20 08:45:57 +0100

    fix : Corriger le when en testant correctement le type

--- kotlin-helloworld/src/controlFlow.kt
+++ kotlin-helloworld/src/controlFlow.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
     val maximum = max(1, 2)
     println("maximum is $maximum")
+    println("isNumber = " + isNumber(1))
+    println("isNumber = " + isNumber("1"))
 }
 
 
@@ -8,6 +10,6 @@ fun max(a: Int, b: Int) = if (a > b) a else b
 
 
 fun isNumber(a: Any): Boolean = when (a) {
-    a is Int -> true
+    is Int -> true
     else -> false
 }

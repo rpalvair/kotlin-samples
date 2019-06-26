8c434c6b36708100b386ac5c66e5687ec83ca112
ruddy palvair <r.palvair@gmail.com>
2019-06-26 18:39:14 +0200

    feat : Heritage avec constructeur primaire

---
+++ kotlin-helloworld/src/com/palvair/kotlin/classes/inheritance/Base.kt
@@ -0,0 +1,3 @@
+package com.palvair.kotlin.classes.inheritance
+
+open class Base(p: Int)
---
+++ kotlin-helloworld/src/com/palvair/kotlin/classes/inheritance/DerivedPrimaryConstuctor.kt
@@ -0,0 +1,3 @@
+package com.palvair.kotlin.classes.inheritance
+
+class DerivedPrimaryConstuctor(p: Int) : Base(p)

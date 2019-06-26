55e1ae422818e9e7954c48ee574deb487ef6370c
ruddy palvair <r.palvair@gmail.com>
2019-06-26 18:47:15 +0200

    feat : Surcharge de méthodes uniquement 'open'

--- kotlin-helloworld/src/com/palvair/kotlin/classes/inheritance/Base.kt
+++ kotlin-helloworld/src/com/palvair/kotlin/classes/inheritance/Base.kt
@@ -1,3 +1,7 @@
 package com.palvair.kotlin.classes.inheritance
 
-open class Base(p: Int)
+open class Base(p: Int) {
+    fun closed() {}
+
+    open fun opened() {}
+}
---
+++ kotlin-helloworld/src/com/palvair/kotlin/classes/inheritance/Derived.kt
@@ -0,0 +1,8 @@
+package com.palvair.kotlin.classes.inheritance
+
+class Derived(p:Int):Base(p) {
+
+    override fun closed() {}
+
+    override fun opened() {}
+}

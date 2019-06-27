ae654863401234eb0cf4d3c5642e9fd934338612
ruddy palvair <r.palvair@gmail.com>
2019-06-27 13:15:58 +0200

    feat : Surcharge de propriété

---
+++ kotlin-helloworld/src/com/palvair/kotlin/classes/inheritance/Bar.kt
@@ -0,0 +1,6 @@
+package com.palvair.kotlin.classes.inheritance
+
+class Bar : Foo() {
+
+    override val x: Int = super.x + 1
+}
---
+++ kotlin-helloworld/src/com/palvair/kotlin/classes/inheritance/Foo.kt
@@ -0,0 +1,6 @@
+package com.palvair.kotlin.classes.inheritance
+
+open class Foo {
+
+    open val x: Int get() = x
+}
